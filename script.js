d93c2a9dc44b82ff70c35361ff4b2568ee0daf76
Joseph5 <153535704+jos3ph1205@users.noreply.github.com>
2024-01-20 17:46:50 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ let winScroll = window.scrollY;
 const navBar = document.getElementById("navbar");
 const navWrapper = document.getElementById("nav");
 
+
+
+// FUNCTIONS
 function scrollToTargetAdjusted(href){
     // console.log(href);
 
@@ -17,21 +20,24 @@ function scrollToTargetAdjusted(href){
     });
 }
 
-const timelineEvent = document.querySelectorAll('[data-yr-btn]').forEach(anchor => {
-    let href = anchor.getAttribute('data-yr-btn');
-    anchor.addEventListener("click", () => {
-        scrollToTargetAdjusted(href);
-    })
-})
-
 function toggleClass(className, elementID) {
     document.getElementById(elementID).classList.toggle(className);
 }
 
-console.log(event);
+function showScrollBar() {   
+    let winScroll = window.scrollY;
+    const infoToggled = document.getElementById("infolink").classList.contains("toggled");
+
+    if (winScroll > 0) {
+        navBar.style.top = "0";
+    } else if (winScroll == 0 & infoToggled == false) {
+        navBar.style.top = "-50px";
+    }
+}
 
 
-// Navbar Top Edge Hover
+
+// NAVBAR TOP EDGE HOVER TRIGGER
 navWrapper.addEventListener("mouseenter", (event) => {
     const infoToggled = document.getElementById("infolink").classList.contains("toggled");
     let winScroll = window.scrollY;
@@ -50,22 +56,40 @@ navWrapper.addEventListener("mouseenter", (event) => {
     })
 })
 
-function showScrollBar() {   
-    let winScroll = window.scrollY;
-    const infoToggled = document.getElementById("infolink").classList.contains("toggled");
-
-    if (winScroll > 0) {
-        navBar.style.top = "0";
-    } else if (winScroll == 0 & infoToggled == false) {
-        navBar.style.top = "-50px";
-    }
-}
 
 
-// addEventListener("resize", () => {
-//     
-// });
+// TOGGLES & CLICK FUNCTIONS
+const yrSbToggleBtns = document.querySelectorAll('[data-yr-sb-toggle]').forEach(btn => {
+    btn.addEventListener("click", () => {
+        toggleClass('hide-yr-sidebar','yr-sidebar');
+    })
+})
 
+const infoDropdownOptionClearToggleWin = document.querySelectorAll('[data-info-dd-option]').forEach(btn => {
+    btn.addEventListener("click", () => {
+        toggleClass('info-dropdown-toggled', 'infoWin');
+        toggleClass('toggled', 'infolink');
+    })
+})
+
+const infoDropdownToggle = document.querySelector('[data-info-dd-toggle]').addEventListener("click", () => {
+    toggleClass('info-dropdown-toggled', 'infoWin');
+    toggleClass('toggled', 'infolink');
+})
+
+const timelineEvent = document.querySelectorAll('[data-yr-btn]').forEach(anchor => {
+    let href = anchor.getAttribute('data-yr-btn');
+    anchor.addEventListener("click", () => {
+        scrollToTargetAdjusted(href);
+    })
+})
+
+
+
+
+
+
+// WINDOW TRIGGER EVENTS
 addEventListener("load", () => {
     showScrollBar();
 });
